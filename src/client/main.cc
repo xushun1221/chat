211c6a25176e69e2278f5c7d489771bffbb90959
xushun <xushun1221@outlook.com>
2022-12-02 01:02:04 -0800

    add client

---
+++ src/client/main.cc
@@ -0,0 +1,12 @@
+/*
+ *  @Filename : main.cc
+ *  @Description : ChatClient Entry
+ *  @Datatime : 2022/12/02 17:00:39
+ *  @Author : xushun
+ */
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
